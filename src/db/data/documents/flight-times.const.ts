784b9382056129994e50ecc12001dc7147c59e43
Christopher <chrisberry4545@googlemail.com>
2018-02-18 23:26:01 +0000

    Added id to static flight times

--- src/db/data/documents/flight-times.const.ts
+++ src/db/data/documents/flight-times.const.ts
@@ -4,9 +4,11 @@ import {
 
 export const FLIGHT_TIMES: {[key: string]: FlightTimesInterface} = {
   ONE_TO_TWO: {
+    _id: '1',
     name: '1 - 2 hours',
   },
   TWO_TO_THREE: {
+    _id: '2',
     name: '2 - 4',
   },
 };
