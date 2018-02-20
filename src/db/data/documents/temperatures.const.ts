5b6357286497765257a94a3baca771b5ea4f2521
Christopher <chrisberry4545@googlemail.com>
2018-02-20 21:04:01 +0000

    Added ids to temperatures

--- src/db/data/documents/temperatures.const.ts
+++ src/db/data/documents/temperatures.const.ts
@@ -4,15 +4,19 @@ import {
 
 export const TEMPERATURES: {[key: string]: TemperatureInterface} = {
   LESS_THAN_TEN: {
+    _id: '0',
     name: '< 10 degrees',
   },
   TEN_TO_TWENTY: {
+    _id: '1',
     name: '10-20 degrees',
   },
   THIRTY_PLUS: {
+    _id: '2',
     name: '30 plus',
   },
   TWENTY_TO_THIRTY: {
+    _id: '3',
     name: '20-30 degrees',
   },
 };
