76f114a92b51a660d59544d6da17c6f964fcb2a1
Christopher <chrisberry4545@googlemail.com>
2018-02-20 21:00:17 +0000

    Added ids to cost ranges

--- src/db/data/documents/cost-ranges.const.ts
+++ src/db/data/documents/cost-ranges.const.ts
@@ -4,16 +4,19 @@ import {
 
 export const COST_RANGES: {[key: string]: CostRangeInterface} = {
   CHEAP: {
+    _id: '0',
     maxCost: 400,
     minCost: 0,
     name: 'Less than £400',
   },
   EXPENSIVE: {
+    _id: '1',
     maxCost: 1000,
     minCost: 400,
     name: '£400 - £1000',
   },
   MODERATE: {
+    _id: '2',
     maxCost: 99999,
     minCost: 1000,
     name: '£1000+',
