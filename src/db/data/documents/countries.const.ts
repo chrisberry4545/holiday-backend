3f9749a0366969e86ec62dfdefad01e153048f33
Christopher <chrisberry4545@googlemail.com>
2018-03-07 14:30:30 +0000

    Added highlights and facts to holidays

--- src/db/data/documents/countries.const.ts
+++ src/db/data/documents/countries.const.ts
@@ -15,6 +15,10 @@ export const COUNTRIES: {[key: string]: CountryInterface} = {
   CHINA: {
     _id: '3',
     continent: CONTINENTS.ASIA,
+    facts: [
+      'China is the highest population of any country in the world',
+      'China has 4 times as many people as the USA',
+    ],
     foodScore: 10,
     foodTypes: [FOOD_TYPES.MACDONALDS, FOOD_TYPES.SPICY],
     monthlyTemperatures: {
@@ -36,6 +40,13 @@ export const COUNTRIES: {[key: string]: CountryInterface} = {
   COSTA_RICA: {
     _id: '1',
     continent: CONTINENTS.CENTRAL_AMERICA,
+    facts: [
+      'Sloths live here',
+      'Costa ricans calls themselves Ticos and Ticas',
+      'The avereage life expectancy is 77 years. This is one of' +
+        ' the highest in the world',
+      'Costa Rica has no standing army',
+    ],
     foodScore: 7,
     foodTypes: [FOOD_TYPES.MACDONALDS, FOOD_TYPES.MEDITERRANEAN],
     monthlyTemperatures: {
@@ -57,6 +68,9 @@ export const COUNTRIES: {[key: string]: CountryInterface} = {
   MEXICO: {
     _id: '2',
     continent: CONTINENTS.CENTRAL_AMERICA,
+    facts: [
+      'The offical name for Mexico is the United Mexican States',
+    ],
     foodScore: 10,
     foodTypes: [FOOD_TYPES.MACDONALDS, FOOD_TYPES.MIDDLE_EASTERN],
     monthlyTemperatures: {
--- src/db/data/documents/holidays.const.ts
+++ src/db/data/documents/holidays.const.ts
@@ -16,6 +16,17 @@ export const HOLIDAYS: {[key: string]: HolidayInterface} = {
       cost: 100,
       flightTime: FLIGHT_TIMES.ONE_TO_TWO,
     },
+    highlights: [{
+      _id: '1',
+      // tslint:disable-next-line
+      imageUrl: 'http://static4.businessinsider.com/image/55c2095c2acae70f008bd411-3008-1504/great%20wall.jpg',
+      title: 'The Great Wall of China',
+    }, {
+      _id: '2',
+      // tslint:disable-next-line
+      imageUrl: 'https://cdn.audleytravel.com/-/-/79/245092040066210246084134084120024103122218203223.jpg',
+      title: 'The Terracotta Army',
+    }],
     name: 'China holiday',
   },
   COSTA_RICA: {
@@ -26,6 +37,12 @@ export const HOLIDAYS: {[key: string]: HolidayInterface} = {
       cost: 600,
       flightTime: FLIGHT_TIMES.ONE_TO_TWO,
     },
+    highlights: [{
+      _id: '3',
+      // tslint:disable-next-line
+      imageUrl: 'https://quostudenttravel.com/wp-content/uploads/2015/08/CostaRica_RainforestWaterfall-Bright.jpg',
+      name: 'Rainforest',
+    }],
     name: 'Costa Rica holiday',
   },
   MEXICO: {
@@ -36,6 +53,13 @@ export const HOLIDAYS: {[key: string]: HolidayInterface} = {
       cost: 500,
       flightTime: FLIGHT_TIMES.ONE_TO_TWO,
     },
+    highlights: [{
+      _id: '3',
+      description: 'Mexican food is some of the best in the world',
+      // tslint:disable-next-line
+      imageUrl: 'http://del.h-cdn.co/assets/16/32/980x490/landscape-1470763907-jack-box-tacos.jpg',
+      title: 'Amazing food',
+    }],
     name: 'Mexico holiday',
   },
 };
