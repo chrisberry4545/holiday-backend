4d07a90463ca5f911b5fd63b691e7b5acec884fc
Christopher <chrisberry4545@googlemail.com>
2018-03-13 09:25:15 +0000

    Temporarily removed mongodb

--- src/db/utilities/connect-db.ts
+++ src/db/utilities/connect-db.ts
@@ -9,12 +9,14 @@ const DB_NAME = 'holiday-db';
 
 export const connectDb = (): Promise<Db> => {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(url, (connectError, db) => {
-      if (connectError) {
-        reject(connectError);
-        throw connectError;
-      }
-      resolve(db.db(DB_NAME));
-    });
+    // Temporarily go without DB
+    resolve(null);
+    // MongoClient.connect(url, (connectError, db) => {
+    //   if (connectError) {
+    //     reject(connectError);
+    //     throw connectError;
+    //   }
+    //   resolve(db.db(DB_NAME));
+    // });
   });
 };
