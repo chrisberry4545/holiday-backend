f6cc46d75cc1b45057e93efd6763a543c1dc4c49
Christopher <chrisberry4545@googlemail.com>
2018-04-07 18:48:58 +0100

    Updated continent list and changed init db to skip holidays and countries

--- src/db/data/documents/continent.const.ts
+++ src/db/data/documents/continent.const.ts
@@ -3,10 +3,28 @@ import {
 } from '@chrisb-dev/holiday-shared-models/';
 
 export const CONTINENTS: {[key: string]: ContinentInterface} = {
+  AFRICA: {
+    _id: '1',
+    name: 'Africa',
+  },
   ASIA: {
+    _id: '2',
     name: 'Asia',
   },
-  CENTRAL_AMERICA: {
-    name: 'Central America',
+  AUSTRALIA: {
+    _id: '3',
+    name: 'Australia',
+  },
+  EUROPE: {
+    _id: '4',
+    name: 'Europe',
+  },
+  NORTH_AMERICA: {
+    _id: '5',
+    name: 'North America',
+  },
+  SOUTH_AMERICA: {
+    _id: '6',
+    name: 'South America',
   },
 };
--- src/db/init-db/index.ts
+++ src/db/init-db/index.ts
@@ -6,11 +6,16 @@ import {
 } from './../utilities';
 
 import {
+  COLLECTIONS,
   DOCUMENT_MAPPINGS,
 } from './../data';
 
 connectDb().then((db) => {
-  DOCUMENT_MAPPINGS.forEach((mapping) => {
+  DOCUMENT_MAPPINGS
+  .filter((mapping) => (
+    mapping.collectionName !== COLLECTIONS.COUNTIRES &&
+    mapping.collectionName !== COLLECTIONS.HOLIDAYS
+  )).forEach((mapping) => {
     dropCollection(db, mapping.collectionName).then(() => {
       createDocuments(db, mapping.data, mapping.collectionName);
     });
